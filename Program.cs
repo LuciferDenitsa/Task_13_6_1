c807ddf52929a854dd1c1c0a15c89cf0560dd948
Daniil <rychenkov16@yandex.ru>
2024-10-12 14:55:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> listWords = new List<string>();
+        LinkedList<string> linkedListWords = new LinkedList<string>();
+        string[] words;
+
+        string path = @"C:\Users\ryche\Desktop\Text_from_Task.txt";
+        using (var sr = new StreamReader(path))
+        {
+            var text = sr.ReadToEnd().ToLower();
+
+            text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            words = text.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+        var watch1 = Stopwatch.StartNew();
+        foreach (var word in words)
+            listWords.Add(word);
+        Console.WriteLine($"Вставка в List<T> заняла: {watch1.Elapsed.TotalMilliseconds} мс");
+
+
+        Console.WriteLine();
+
+
+        linkedListWords.AddFirst("1");
+        var watch2 = Stopwatch.StartNew();
+        foreach (var word in words)
+            linkedListWords.AddAfter(linkedListWords.First, word);
+        Console.WriteLine($"Вставка в LinkedList<T> заняла: {watch2.Elapsed.TotalMilliseconds} мс");
+
+        Console.ReadKey();
+    }
+
+}
